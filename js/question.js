20dabf175690121b3a0a8251706c61c16775c11c
KoyaIdo <ko80410@icloud.com>
2022-01-24 16:36:29 +0000

    demo

--- js/question.js
+++ js/question.js
@@ -114,90 +114,90 @@ else{alert('はずれ!');}
 
 function T1(){
     add();
-    document.getElementById('question1').innerHTML='Q1.答え: M [MOKU(木曜日)]';
+    document.getElementById('question1').innerHTML='<p class="kotae">答え: M [MOKU(木曜日)]</p>';
 }
 
 function T2(){
     add();
-    document.getElementById('question2').innerHTML='Q2.答え: S [six(6)]';
+    document.getElementById('question2').innerHTML='<p class="kotae">答え: S [six(6)]</p>';
 }
 
 function T3(){
     add();
-    document.getElementById('question3').innerHTML='Q3.答え: E [Seven-Eleven]';
+    document.getElementById('question3').innerHTML='<p class="kotae">答え: E [Seven-Eleven]</p>';
 }
 
 function T4(){
     add();
-    document.getElementById('question4').innerHTML='Q4.答え: D [Day(日)]';
+    document.getElementById('question4').innerHTML='<p class="kotae">答え: D [Day(日)]</p>';
 }
 
 function T5(){
     add();
-    document.getElementById('question5').innerHTML='Q5.答え: S [SHOUWA(昭和)]';
+    document.getElementById('question5').innerHTML='<p class="kotae">答え: S [SHOUWA(昭和)]</p>';
 }
 
 function T6(){
     add();
-    document.getElementById('question6').innerHTML='Q6.答え: K [KYOU(今日)]';
+    document.getElementById('question6').innerHTML='<p class="kotae">答え: K [KYOU(今日)]</p>';
 }
 
 function T7(){
     add();
-    document.getElementById('question7').innerHTML='Q7.答え: J [January(１月),June(６月),July(７月)]';
+    document.getElementById('question7').innerHTML='<p class="kotae">答え: J [January(１月),June(６月),July(７月)]</p>';
 }
 
 function T8(){
     add();
-    document.getElementById('question8').innerHTML='Q8.答え: 43 [43県]';
+    document.getElementById('question8').innerHTML='<p class="kotae">答え: 43 [43県]</p>';
 }
 
 function T9(){
     add();
-    document.getElementById('question9').innerHTML='Q9.答え: K [Km(キロメートル)]';
+    document.getElementById('question9').innerHTML='<p class="kotae">答え: K [Km(キロメートル)]</p>';
 }
 
 function T10(){
     add();
-    document.getElementById('question10').innerHTML='Q10.答え: Y [YEN(円)]';
+    document.getElementById('question10').innerHTML='<p class="kotae">答え: Y [YEN(円)]</p>';
 }
 
 function HINT1(){
-    document.getElementById('hint1').innerHTML='ヒント: 一週間'
+    document.getElementById('hint1').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> 一週間</span></div>';
 }
 
 function HINT2(){
-    document.getElementById('hint2').innerHTML='ヒント: １〜１０'
+    document.getElementById('hint2').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> １〜１０</span></div>'
 }
 
 function HINT3(){
-    document.getElementById('hint3').innerHTML='ヒント: よく見かけるコンビニの名前'
+    document.getElementById('hint3').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> よく見かけるコンビニの名前</span></div>'
 }
 
 function HINT4(){
-    document.getElementById('hint4').innerHTML='ヒント: 時間の...'
+    document.getElementById('hint4').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> 時間の...</span></div>'
 }
 
 function HINT5(){
-    document.getElementById('hint5').innerHTML='ヒント: 今はRです。'
+    document.getElementById('hint5').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> 今はRです。</span></div>'
 }
 
 function HINT6(){
-    document.getElementById('hint6').innerHTML='ヒント: ２日前から３日後を...'
+    document.getElementById('hint6').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> ２日前から３日後を...</span></div>'
 }
 
 function HINT7(){
-    document.getElementById('hint7').innerHTML='ヒント: 12ある何かの頭文字'
+    document.getElementById('hint7').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> 12ある何かの頭文字</span></div>'
 }
 
 function HINT8(){
-    document.getElementById('hint8').innerHTML='ヒント: 日本...'
+    document.getElementById('hint8').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> 日本...</span></div>'
 }
 
 function HINT9(){
-    document.getElementById('hint9').innerHTML='ヒント: 距離の単位'
+    document.getElementById('hint9').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> 距離の単位</span></div>'
 }
 
 function HINT10(){
-    document.getElementById('hint10').innerHTML='ヒント: たくさん欲しいよね...。'
+    document.getElementById('hint10').innerHTML='<div><span class="iro1">ヒント:</span><span class="hint"> たくさん欲しいよね...。</span></div>'
 }
